f9e8b4bef8ba58285689c38d4d1deadd50755bca
AS <ascii2017r@gmail.com>
2021-10-02 15:46:39 +0200

    using format method

---
+++ Using the format method.py
@@ -0,0 +1,5 @@
+message = 'My name is {0:s} and I am {1:d} years old'.format('Paul', 35)
+
+#the first number inside the bracks {} refers to the position. After the column we place the formatter
+#if you do not watn to format the string simply delete the content innside the brackets {}
+print(message)
